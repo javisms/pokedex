3740ff343d6d6b5b3a7be3c45789ef8b6df29310
Javier Angel <javierangel@Javiers-MacBook-Pro.local>
2016-05-20 18:46:25 -0400

    set up cellcollectionview for images

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Javier Angel on 5/20/16.
+//  Copyright © 2016 Iohta Group. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Javier Angel on 5/20/16.
+//  Copyright © 2016 Iohta Group. All rights reserved.
+//
+
+import Foundation
